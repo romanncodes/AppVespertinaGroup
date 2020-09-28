4805f9fd5deb9f6751ff6790e5d201c1fb5baea2
Roman <roman.gajardo@gmail.com>
2020-09-28 19:28:59 -0300

    creacion del modelo

---
+++ app/src/main/java/cl/roman/appvespertinagroup/Modelo.java
@@ -0,0 +1,17 @@
+package cl.roman.appvespertinagroup;
+
+public class Modelo {
+    public String name;
+    public String lastName;
+    public int age;
+
+    public Modelo(String name, String lastName, int age) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public static String getConfig(){
+        return "CONFIG";
+    }
+}
